f27dbcf78cb88fd293a881e6e0f29e9b1edb2ded
unknown <kimberly.zai@berkeley.edu>
2017-04-27 15:23:34 -0700

    Run_tests.py file now works. Run from the project_instances folder

---
+++ project_instances/run_tests.py
@@ -0,0 +1,10 @@
+import os
+
+if __name__ == '__main__':
+    file_name = "problem"
+
+    for i in range(1, 22):
+        input_file = file_name + str(i)
+        output_file = file_name + str(i) + ".out"
+        print('sample_solver_constraints.py ' + input_file + " " + output_file)
+        os.system('sample_solver_constraints.py ' + input_file + " " + output_file)
